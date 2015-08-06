b63daa3386e154ac4430fe6a41493bf4c850ca76
Janice Laset-Parkerson & Brigham Thompson <student@epicodus.com>
2015-08-06 12:03:02 -0700

    add spec to return more than one coin

--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -10,4 +10,8 @@ describe('Fixnum#coin_combo') do
   it('return a combination of coins') do
     expect((30).coin_combo()).to(eq("quarter, nickel"))
   end
+
+  it('returns coin quantity') do
+    expect((50).coin_combo()).to(eq("2 quarter"))
+  end
 end
