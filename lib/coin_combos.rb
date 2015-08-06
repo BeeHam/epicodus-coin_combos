f1b07ebe6680265ceeccf614a89f5947816b3de6
Janice Laset-Parkerson & Brigham Thompson <student@epicodus.com>
2015-08-06 11:57:12 -0700

    return coin pair

--- lib/coin_combos.rb
+++ lib/coin_combos.rb
@@ -1,12 +1,37 @@
-class Float
+# require('pry')
+class Fixnum
   define_method(:coin_combo) do
-    coins = {0.25 => "quarter",
-             0.10 => "dime",
-             0.05 => "nickel",
-             0.01 => "penny"}
-
-    if coins.include?(self)
-      coins.fetch(self)
+    coins = {25 => "quarter",
+             10 => "dime",
+             5 => "nickel",
+             1 => "penny"}
+    amount = self
+    result = String.new()
+    quarter = 0
+    nickel = 0
+# binding.pry
+    until amount.==(0)
+      if coins.include?(self)
+        amount = amount - self
+        result = coins.fetch(self)
+      elsif amount >= coins.keys[0]
+        amount = amount.-(coins.keys[0])
+        quarter = quarter + 1
+# binding.pry
+      elsif amount >= coins.keys[1]
+        amount = amount.-(coins.keys[1])
+      else amount >= coins.keys[2]
+        amount = amount.-(coins.keys[2])
+        nickel = nickel + 1
+      end
     end
+
+    if quarter > 0
+      result.concat(coins.fetch(25) + ', ')
+    end
+    if nickel > 0
+      result.concat(coins.fetch(5))
+    end
+    result
   end
 end
--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -1,12 +1,13 @@
 require('rspec')
 require('coin_combos')
 
-describe('Float#coin_combo') do
+
+describe('Fixnum#coin_combo') do
   it('return single coin') do
-    expect((0.10).coin_combo()).to(eq("dime"))
+    expect((10).coin_combo()).to(eq("dime"))
   end
 
   it('return a combination of coins') do
-    expect((0.30).coin_combo()).to(eq("1 quarter, 1 nickel"))
+    expect((30).coin_combo()).to(eq("quarter, nickel"))
   end
 end
