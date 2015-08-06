06410497cb7cd8c92cc1e7be6c2ccecc4dfaabfb
Janice Laset-Parkerson & Brigham Thompson <student@epicodus.com>
2015-08-06 10:56:59 -0700

    add spec to return combination of coins

--- spec/coin_combos_spec.rb
+++ spec/coin_combos_spec.rb
@@ -5,4 +5,8 @@ describe('Float#coin_combo') do
   it('return single coin') do
     expect((0.10).coin_combo()).to(eq("dime"))
   end
+
+  it('return a combination of coins') do
+    expect((0.30).coin_combo()).to(eq("1 quarter, 1 nickel"))
+  end
 end
